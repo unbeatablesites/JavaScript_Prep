d53d3150c7de7df09cb9e8eb1dee67eb16fd940d
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-19 12:44:02 -0500

    done

--- DataTypes/script.js
+++ DataTypes/script.js
@@ -1,6 +1,3 @@
-var johnsTeamScores = johnGame1 + johnGame2 + johnGame3;
-var mikesTeamScores = mikesGame1 + mikesGame2 + mikesGame3;
-
 var johnGame1 = 89;
 var johnGame2 = 120;
 var johnGame3 = 103;
@@ -9,19 +6,34 @@ var mikesGame1 = 116;
 var mikesGame2 = 94;
 var mikesGame3 = 123;
 
+var maryGame1 = 97;
+var maryGame2 = 134;
+var maryGame3 = 105;
+
+var johnsTeamScores = johnGame1 + johnGame2 + johnGame3;
+var mikesTeamScores = mikesGame1 + mikesGame2 + mikesGame3;
+var marysTeamScores = maryGame1 + maryGame2 + maryGame3;
+
 var mikeAverage = mikesTeamScores / 3;
 var johnAverage = johnsTeamScores / 3;
+var maryAverage = marysTeamScores / 3;
 
-if (mikeAverage > johnAverage) {
+if (mikeAverage > johnAverage && mikeAverage > maryAverage) {
   console.log(
-    "Mikes's team has the higher average!" + mikeAverage + 'on average'
+    "Mikes's team has the higher average! " + mikeAverage + ' on average'
   );
-} else if (johnAverage > mikeAverage) {
+} else if (johnAverage > mikeAverage && johnAverage > maryAverage) {
   console.log(
     "John's team has the higher average! They scored " +
       johnAverage +
-      'on average'
+      ' on average'
+  );
+} else if (maryAverage > mikeAverage && maryAverage > johnAverage) {
+  console.log(
+    "John's team has the higher average! They scored " +
+      maryAverage +
+      ' on average'
   );
 } else {
-  console.log('Both teams tied in average');
+  console.log('Two or more teams tied in average');
 }
