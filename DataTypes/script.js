81f9419b5d0baf8c998ae10f6148f61252c2ea91
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-19 15:02:28 -0500

    Switch inside function

--- DataTypes/script.js
+++ DataTypes/script.js
@@ -1,3 +1,12 @@
 // function whatDoYouDo(job, firstName) {}
 
-var whatDoYouDo = function(job, firstName) {};
+var whatDoYouDo = function(job, firstName) {
+  switch (job) {
+    case 'Teacher':
+      console.log('I teach students');
+      break;
+    case 'Developer':
+      console.log('I make apps');
+      break;
+  }
+};
