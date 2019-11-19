066f85e40a12df611577cf683c8cf0726637f48b
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-19 14:46:13 -0500

    functions

--- DataTypes/script.js
+++ DataTypes/script.js
@@ -2,8 +2,22 @@ function calculateAge(birthYear) {
   return 2018 - birthYear;
 }
 
-var age = calculateAge(1984);
-var agemike = calculateAge(1985);
-var ageMary = calculateAge(1986);
+// var ageFrank = calculateAge(1984);
+// var agemike = calculateAge(1985);
+// var ageMary = calculateAge(1986);
+// var now = 2019;
 
-console.log(age, ageMary, agemike);
+// console.log(ageFrank, ageMary, agemike);
+
+function yearsUnitlRetirement(birthYear, firstName) {
+  var age = calculateAge(birthYear);
+  var retirmentAge = 65 - age;
+
+  if (retirmentAge < 0) {
+    console.log(firstName + ' Has already retired');
+  } else {
+    console.log(firstName + ' will retire in ' + retirmentAge + ' years');
+  }
+}
+
+yearsUnitlRetirement(1950, 'Frank');
