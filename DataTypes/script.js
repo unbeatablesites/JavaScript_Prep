5e2901b59e6bf71df1c6ba4cfe17cbc5191b0356
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-19 12:33:58 -0500

    Working on logic

--- DataTypes/script.js
+++ DataTypes/script.js
@@ -8,3 +8,18 @@ var johnGame3 = 103;
 var mikesGame1 = 116;
 var mikesGame2 = 94;
 var mikesGame3 = 123;
+
+var mikeAverage = mikesTeamScores / 3;
+var johnAverage = johnsTeamScores / 3;
+
+if (mikeAverage > johnAverage) {
+  console.log(
+    "Mikes's team has the higher average!" + mikeAverage + 'on average'
+  );
+} else {
+  console.log(
+    "John's team has the higher average! They scored " +
+      johnAverage +
+      'on average'
+  );
+}
