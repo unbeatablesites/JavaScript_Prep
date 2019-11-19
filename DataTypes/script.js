16e35dd61808bf288dc7627d5b393d0e2e344227
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-19 11:34:55 -0500

    if/else

--- DataTypes/script.js
+++ DataTypes/script.js
@@ -1,19 +1,28 @@
-var johnsHeight, marksHeight, johnsMass, markMass;
+var firstName = 'Frank';
+var status = 'married';
 
-marksHeight = 1.69;
-markMass = 78;
-johnsMass = 92;
-johnsHeight = 1.95;
-
-johnBMI = johnsMass / (johnsHeight * johnsHeight);
-markBMI = markMass / (marksHeight * marksHeight);
-
-console.log(johnBMI + ' John');
-console.log(markBMI + ' Mark');
-// BMI = mass/ height * height;
-
-if (markBMI > johnBMI) {
-  console.log("Mark's BMI is greater");
+if (status) {
+  console.log('Is ' + firstName + ' Married ' + true);
 } else {
-  console.log("John's Bmi is greater");
+  console.log('Will mary soon');
 }
+
+// var johnsHeight, marksHeight, johnsMass, markMass;
+
+// marksHeight = 1.69;
+// markMass = 78;
+// johnsMass = 92;
+// johnsHeight = 1.95;
+
+// johnBMI = johnsMass / (johnsHeight * johnsHeight);
+// markBMI = markMass / (marksHeight * marksHeight);
+
+// console.log(johnBMI + ' John');
+// console.log(markBMI + ' Mark');
+// // BMI = mass/ height * height;
+
+// if (markBMI > johnBMI) {
+//   console.log("Mark's BMI is greater");
+// } else {
+//   console.log("John's Bmi is greater");
+// }
