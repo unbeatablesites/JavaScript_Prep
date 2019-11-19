d371ba173f2245ce6a03d29f50aa26df8883ff82
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-19 16:11:32 -0500

    Tip calculator

--- DataTypes/script.js
+++ DataTypes/script.js
@@ -1,16 +1,18 @@
-function tipCalculator(bill) {}
+function tipCalculator(bill) {
+  var percentage;
+
+  if (familyBill < 50) {
+    percentage = 0.2;
+  } else if (familyBill <= 50 && familyBill < 200) {
+    console.log(familyBill + familyBill * 0.15);
+  } else {
+    console.log(familyBill + familyBill * 0.1);
+  }
+}
 
 // var familyBill = [124, 48, 268];
 
 // var allThreeTips = function(familyBill)
-
-// // if (familyBill < 50) {
-// //   console.log(familyBill + familyBill * 0.2);
-// // } else if (familyBill <= 50 && familyBill < 200) {
-// //   console.log(familyBill + familyBill * 0.15);
-// // } else {
-// //   console.log(familyBill + familyBill * 0.1);
-// // }
 
 // console.log(familyBill);
 
