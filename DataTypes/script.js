51de74b662acc4f95252e7dae8220d95acef2677
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-19 12:01:15 -0500

    Switch statement

--- DataTypes/script.js
+++ DataTypes/script.js
@@ -1,48 +1,15 @@
+var job = 'cop';
 var firstName = 'Frank';
-var age = 35;
 
-var drink = age > 18 ? 'can drink beer' : 'can drink juice';
+switch (job) {
+  case 'developer':
+    console.log(firstName + ' makes software');
+    break;
 
-console.log(firstName + ' ' + drink);
+  case 'teacher':
+    console.log(firstName + ' teaches students');
+    break;
 
-// var firstName = 'Frank';
-// var age = 35;
-
-// if (age < 13) {
-//   console.log(firstName + ' is a boy');
-// } else if (age >= 13 && age < 20) {
-//   console.log(firstName + ' is a teenager');
-// } else if (age >= 20 && age < 30) {
-//   console.log(firstName + ' is a young man');
-// } else {
-//   console.log(firstName + ' is a man');
-// }
-
-// var firstName = 'Frank';
-// var status = 'married';
-
-// if (status) {
-//   console.log('Is ' + firstName + ' Married ' + true);
-// } else {
-//   console.log('Will mary soon');
-// }
-
-// var johnsHeight, marksHeight, johnsMass, markMass;
-
-// marksHeight = 1.69;
-// markMass = 78;
-// johnsMass = 92;
-// johnsHeight = 1.95;
-
-// johnBMI = johnsMass / (johnsHeight * johnsHeight);
-// markBMI = markMass / (marksHeight * marksHeight);
-
-// console.log(johnBMI + ' John');
-// console.log(markBMI + ' Mark');
-// // BMI = mass/ height * height;
-
-// if (markBMI > johnBMI) {
-//   console.log("Mark's BMI is greater");
-// } else {
-//   console.log("John's Bmi is greater");
-// }
+  default:
+    console.log('not sure what you do');
+}
