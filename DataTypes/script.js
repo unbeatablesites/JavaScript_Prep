d3f0aeb805355312f87adb4f800209f4d8f404f0
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-19 15:27:39 -0500

    Arrays

--- DataTypes/script.js
+++ DataTypes/script.js
@@ -1,9 +1,9 @@
-var names = ['Frank', 'Mike', 'John'];
-var years = [1984, 1969, 1948];
+// var names = ['Frank', 'Mike', 'John'];
+// var years = [1984, 1969, 1948];
 
-console.log(names[0]);
-console.log(names.length);
+// console.log(names[0]);
+// console.log(names.length);
 
-names[1] = 'Mark';
-names[names.length] = 'Mary';
-console.log(names);
+// names[1] = 'Mark';
+// names[names.length] = 'Mary';
+// console.log(names);
